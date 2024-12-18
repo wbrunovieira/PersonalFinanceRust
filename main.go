a2254dc3fe49873232a1709348466505e3cf4675
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-12-18 04:42:45 -0300

    added cors allowed

--- main.go
+++ main.go
@@ -12,11 +12,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	config.InitDB()
 	defer config.DB.Close()
 
 	router := mux.NewRouter()
+
+	router.Use(enableCORS)
 
 	routes.RegisterUserRoutes(router)
 	routes.RegisterTransactionRoutes(router)
