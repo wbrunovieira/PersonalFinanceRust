1632e39e1370eb7ebc15111e9d6545d452c5bffe
Bruno MacM3 <wbrunovieira77@gmail.com>
2024-12-27 17:58:16 -0300

    added cors for network

--- main.go
+++ main.go
@@ -14,7 +14,21 @@ import (
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		allowedOrigins := []string{"http://localhost:5173", "http://192.168.0.9:5173"}
+		origin := r.Header.Get("Origin")
+
+		isAllowed := false
+		for _, o := range allowedOrigins {
+			if origin == o {
+				isAllowed = true
+				break
+			}
+		}
+
+		if isAllowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
